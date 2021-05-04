f21464820ee87e5525f967cff1e31ae09401a50f
Cody Sheridan <sheridan.codyj@gmail.com>
2021-05-04 13:54:16 -0400

    Ex. 1/2 Complete.

---
+++ Milestone1/Milestone1Library/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Milestone1Library
+{
+    public class Cell
+    {
+        public int RowNumber { get; set; } = -1;
+        public int ColumnNumber { get; set; } = -1;
+        public int LiveNeighbors { get; set; }
+        public bool Visited { get; set; }
+        public bool Live { get; set; }
+
+        public Cell(int x, int y)
+        {
+            RowNumber = x;
+            ColumnNumber = y;
+        }
+    }
+}
