ef4bf15a86be69a4ec17953c0e42e15d2096a573
Cody Sheridan <sheridan.codyj@gmail.com>
2021-05-05 08:26:14 -0400

    Variable size console minefield created.

--- Milestone1/Milestone1/Program.cs
+++ Milestone1/Milestone1/Program.cs
@@ -1,13 +1,55 @@
-﻿
-using System;
+﻿using System;
+using System.Linq;
+using Milestone1Library;
 
 namespace Milestone1
 {
     class Program
     {
-        static void Main(string [] args)
+        static Board field = new Board(11);
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Initialize mine field.
+            printBoard(field);
         }
+
+        private static void printBoard(Board field)
+        {
+            // Title block.
+            Console.WriteLine("---------------");
+            Console.WriteLine("| MINESWEEPER |");
+            Console.WriteLine("---------------\n");
+
+            // Column number labels at top of board.
+            Console.Write("+");
+            for (int i = 0; i <= field.Size; i++)
+            {
+                if (i < 10)
+                    Console.Write($" {i} +");
+                else
+                    Console.Write($" {i}+");
+            }
+            Console.WriteLine();
+            
+            // Variable length horizontal cell border.
+            string border = string.Concat(Enumerable.Repeat("+---", field.Size + 1));
+            for (int i = 0; i <= field.Size; i++)
+            {
+                Console.Write(border);
+                Console.WriteLine("+");
+                for (int j = 0; j <= field.Size; j++)
+                {
+                    // Placeholder for bomb.
+                    Console.Write($"| * ");
+                }
+
+                // Row number labels at right of board.
+                Console.WriteLine($"| {i}");
+
+            }
+            Console.Write(border + "+");
+            Console.WriteLine();
+        }
+
     }
 }
--- Milestone1/Milestone1Library/Board.cs
+++ Milestone1/Milestone1Library/Board.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Milestone1Library
 {
-    class Board
+    public class Board
     {
+        public int Size { get; set; }
+        public Cell[,] Grid { get; set; }
+
+        public decimal Difficulty { get; set; }
+
+        public Board(int size)
+        {
+            // Initialize board size.
+            Size = size;
+
+            // Create new 2D array of type Cell.
+            Grid = new Cell [Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Grid [i, j] = new Cell(i, j);
+                }
+            }
+        }
     }
 }
