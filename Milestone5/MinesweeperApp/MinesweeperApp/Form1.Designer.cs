7d38d720510525c7fa0cb888a2c4c45b31685633
Cody Sheridan <sheridan.codyj@gmail.com>
2021-05-18 15:52:32 -0400

    In progress.

---
+++ Milestone5/MinesweeperApp/MinesweeperApp/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace MinesweeperApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblClicks = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsDifficultySetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsDifficultyEasy = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsDifficultyNormal = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsDifficultyHard = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(465, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clicks";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(12, 61);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(27, 32);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "0";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblClicks
+            // 
+            this.lblClicks.AutoSize = true;
+            this.lblClicks.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblClicks.Location = new System.Drawing.Point(480, 61);
+            this.lblClicks.Name = "lblClicks";
+            this.lblClicks.Size = new System.Drawing.Size(27, 32);
+            this.lblClicks.TabIndex = 3;
+            this.lblClicks.Text = "0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(243, 36);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(50, 50);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(513, 375);
+            this.panel1.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(537, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsDifficultySetting,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // tsDifficultySetting
+            // 
+            this.tsDifficultySetting.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsDifficultyEasy,
+            this.tsDifficultyNormal,
+            this.tsDifficultyHard});
+            this.tsDifficultySetting.Name = "tsDifficultySetting";
+            this.tsDifficultySetting.Size = new System.Drawing.Size(122, 22);
+            this.tsDifficultySetting.Text = "Difficulty";
+            // 
+            // tsDifficultyEasy
+            // 
+            this.tsDifficultyEasy.Name = "tsDifficultyEasy";
+            this.tsDifficultyEasy.Size = new System.Drawing.Size(114, 22);
+            this.tsDifficultyEasy.Text = "Easy";
+            // 
+            // tsDifficultyNormal
+            // 
+            this.tsDifficultyNormal.Name = "tsDifficultyNormal";
+            this.tsDifficultyNormal.Size = new System.Drawing.Size(114, 22);
+            this.tsDifficultyNormal.Text = "Normal";
+            // 
+            // tsDifficultyHard
+            // 
+            this.tsDifficultyHard.Name = "tsDifficultyHard";
+            this.tsDifficultyHard.Size = new System.Drawing.Size(114, 22);
+            this.tsDifficultyHard.Text = "Hard";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(119, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(0, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(538, 2);
+            this.label3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 485);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblClicks);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Minesweeper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblClicks;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsDifficultySetting;
+        private System.Windows.Forms.ToolStripMenuItem tsDifficultyEasy;
+        private System.Windows.Forms.ToolStripMenuItem tsDifficultyNormal;
+        private System.Windows.Forms.ToolStripMenuItem tsDifficultyHard;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Milestone5/MinesweeperApp/MinesweeperApp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using MinesweeperLibrary;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MinesweeperApp
+{
+    public partial class Form1 : Form
+    {
+        // Initialize empty mine field.
+        static Board field = new Board();
+
+        // Initialize game timer.
+        private Stopwatch watch = new Stopwatch();
+        private int rows, cols, clicks;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Update display time label.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = watch.Elapsed;
+            string displayTime = string.Format("{0}", ts.Seconds);
+            lblTime.Text = displayTime;
+        }
+
+        // Initialize new game.
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int clicks = 0;
+
+            // Reset time and clicks.
+            watch.Restart();
+            lblClicks.Text = clicks.ToString();
+
+            rows = panel1.Height / field.Size;
+            cols = panel1.Width / field.Size;
+
+            //for (int r = 0; r < rows; r++)
+            //{
+            //    for (int c = 0; c < cols; c++)
+            //    {
+                    
+            //        field.Grid [r, c] = new Button();
+            //        field.Grid [r, c].ColumnNumber = c;
+            //        field.Grid [r, c].RowNumber = r;
+
+                    
+            //    }
+            //}
+        }
+
+    }
+}
